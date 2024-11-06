095315c39eda4ed1b7ec05de81fe9424ef766ec4
Dennis Hickox <frozzel@me.com>
2024-11-06 12:49:43 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,78 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var cors = require('cors');
+
+var indexRouter = require('./routes/index');
+
+var app = express();
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+var corsOptions = {
+  origin: '*'
+};
+app.use(cors(corsOptions));
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+// module.exports = app;
+
+
+// var debug = require('debug')('talking-avatar-backend:server');
+var http = require('http');
+
+/**
+ * Get port from environment and store in Express.
+ */
+
+var PORT = process.env.PORT || 8080;
+// app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+
+var server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(PORT,  () => {// start express server on port 8080
+  console.log(`................................................`)
+  console.log(`🚀  Server running on http://localhost:${PORT}, 🚀`)
+  console.log(`...............................................`)
+  console.log(`...............Starting Database...............`)
+ 
+  
+})
+// server.on('error', onError);
+// server.on('listening', onListening);
+
