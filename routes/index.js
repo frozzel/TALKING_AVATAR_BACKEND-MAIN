c7672d9d3b4734c43cefa1707f3b9a4f19d215be
Dennis Hickox <frozzel@me.com>
2024-11-18 13:36:31 -0500

    basic gpt integration, with present avitar and typing

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,22 @@
 var express = require('express');
-var router = express.Router();
+const router = express.Router();
 var textToSpeech = require('../helpers/tts');
+const {chatGpt}= require('../controllers/chatGpt')
 
 /* GET home page. */
-router.post('/talk', function(req, res, next) {
-
-  textToSpeech(req.body.text, req.body.voice)
-  .then(result => {
-    res.json(result);    
-  })
-  .catch(err => {
-    res.json({});
-  });
+// router.post('/talk', function(req, res, next) {
 
 
-});
+//   textToSpeech(req.body.text, req.body.voice)
+//   .then(result => {
+//     res.json(result);    
+//   })
+//   .catch(err => {
+//     res.json({});
+//   });
+
+
+// });
+router.post('/talk', chatGpt)
 
 module.exports = router;
